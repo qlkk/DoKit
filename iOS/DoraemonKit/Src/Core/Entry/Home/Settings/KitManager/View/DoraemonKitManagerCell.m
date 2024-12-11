84b728c85ffd64425cec609dae641d5b63faac7c
qinxiaokang <qinxiaokang@xinye.com>
2024-12-11 13:32:24 +0800

    xcode16兼容

--- iOS/DoraemonKit/Src/Core/Entry/Home/Settings/KitManager/View/DoraemonKitManagerCell.m
+++ iOS/DoraemonKit/Src/Core/Entry/Home/Settings/KitManager/View/DoraemonKitManagerCell.m
@@ -14,7 +14,7 @@
 @property (nonatomic, strong) UIImageView *icon;
 @property (nonatomic, strong) UILabel *name;
 @property (nonatomic, strong) UIImageView *select;
-@property (nonatomic, strong) UIView *maskView;
+@property (nonatomic, strong) UIView *maskView1;
 
 @end
 
@@ -36,7 +36,7 @@
         self.layer.borderColor = [UIColor doraemon_colorWithHexString:@"#EEEEEE"].CGColor;
         
         [self addSubview:self.centerView];
-        [self addSubview:self.maskView];
+        [self addSubview:self.maskView1];
         [self addSubview:self.select];
         
         [self.centerView addSubview:self.icon];
@@ -89,13 +89,13 @@
     return _select;
 }
 
-- (UIView *)maskView{
-    if (!_maskView) {
-        _maskView = [[UIView alloc] initWithFrame:self.bounds];
-        _maskView.backgroundColor = [UIColor whiteColor];
-        _maskView.alpha = 0.5;
+- (UIView *)maskView1{
+    if (!_maskView1) {
+        _maskView1 = [[UIView alloc] initWithFrame:self.bounds];
+        _maskView1.backgroundColor = [UIColor whiteColor];
+        _maskView1.alpha = 0.5;
     }
-    return _maskView;
+    return _maskView1;
 }
 
 
@@ -106,14 +106,14 @@
         self.select.hidden = NO;
         if (select) {
             self.select.image = [UIImage doraemon_xcassetImageNamed:@"doraemon_check_circle_fill"];
-            self.maskView.hidden = YES;
+            self.maskView1.hidden = YES;
         }else{
             self.select.image = [UIImage doraemon_xcassetImageNamed:@"doraemon_check_circle"];
-            self.maskView.hidden = NO;
+            self.maskView1.hidden = NO;
         }
     }else{
         self.select.hidden = YES;
-        self.maskView.hidden = YES;
+        self.maskView1.hidden = YES;
     }
 }
 
